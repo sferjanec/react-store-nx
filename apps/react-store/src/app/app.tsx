5d216abed537372bae0ea460c5e5caa493791759
georgeisbusting <georgeisbusting@gmail.com>
2024-07-07 10:50:03 -0400

    add router. Update products route

--- apps/react-store/src/app/app.tsx
+++ apps/react-store/src/app/app.tsx
@@ -1,13 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { Route, Routes } from 'react-router-dom';
+// importing the component from the library
+import { Products } from '@react-monorepo/products';
+
+function Home() {
+  return <h1>Home</h1>;
+}
 
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="react-store" />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/products" element={<Products />}></Route>
+    </Routes>
   );
 }
 
